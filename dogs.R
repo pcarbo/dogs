fb7b721c676d6aa091f5fabad732b39dd2873c6f
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-05-21 15:21:05 +0000

    Small edit to dogs.R.

--- dogs.R
+++ dogs.R
@@ -3,9 +3,8 @@ library(cowplot)
 dogs <- read.csv("dogs.csv",stringsAsFactors = FALSE)
 fit  <- lm(AOD ~ weight,dogs)
 b    <- coef(fit)
-ggplot(dogs,aes(x = weight,y = AOD)) +
+print(ggplot(dogs,aes(x = weight,y = AOD)) +
   geom_point(color = "dodgerblue") +
   geom_abline(intercept = b[1],slope = b[2],color = "magenta",
               linetype = "dotted") +
-  theme_cowplot()
-
+  theme_cowplot())
