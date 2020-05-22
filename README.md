319381e815840d514e370779e30728c859ed18c9
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-05-22 16:47:03 -0500

    Made a few revisions to the R code and README.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # dogs
 
-This git repository demonstrates the use of [Binder][binder] for sharing a
-data analysis in R. To begin, simply click this button:
 [![Binder](http://mybinder.org/badge_logo.svg)](http://mybinder.org/v2/gh/pcarbo/dogs/master?urlpath=rstudio).
+
+Minimal illustration of [Binder][binder] for sharing a data analysis in
+R.
+
+To get started, click the "launch binder" button above, then try
+running the code in [dogs.R](dogs.R).
 
 [binder]: https://mybinder.org
--- dogs.R
+++ dogs.R
@@ -1,10 +1,20 @@
 library(ggplot2)
 library(cowplot)
+
+# Read the height, weight and age-of-death (AOD) data for different
+# dog breeds. These data accompany the paper "Single-nucleotide-
+# polymorphism-based association mapping of dog stereotypes" by Jones
+# et al (2008), doi:10.1534/genetics.108.087866.
 dogs <- read.csv("dogs.csv",stringsAsFactors = FALSE)
-fit  <- lm(AOD ~ weight,dogs)
-b    <- coef(fit)
+
+# Fit a line for AOD vs. body weight (in pounds).
+fit <- lm(AOD ~ weight,dogs)
+b   <- coef(fit)
+
+# Plot AOD vs. body weight, along with the best-fit line (dashed, blue
+# line). Compare with Figure 4 from Jones et al (2008).
 print(ggplot(dogs,aes(x = weight,y = AOD)) +
-  geom_point(color = "dodgerblue") +
-  geom_abline(intercept = b[1],slope = b[2],color = "magenta",
-              linetype = "dotted") +
+  geom_point(color = "black") +
+  geom_abline(intercept = b["(Intercept)"],slope = b["weight"],
+              color = "darkorange",linetype = "dashed") +
   theme_cowplot())
